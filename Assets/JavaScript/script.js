7f2169bda06907a8d122b32a8752497650e4cf60
Chris4562022 <williamschris83@gmail.com>
2022-04-14 15:09:38 -0400

    adding Test Files

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,13 @@
+// Objects are a collection of Values
+
+var cars = {
+    make: "Honda",
+    model: "Civic",
+    year: 2003
+};
+
+var displayList = function() {
+    alert("Make: " + cars.make);
+};
+
+displayList();
